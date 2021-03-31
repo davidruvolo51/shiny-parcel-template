ed620224106285b782f7df1681a725fc3587168a
davidruvolo51 <davidruvolo51@gmail.com>
2021-03-31 21:31:12 +0200

    init pkgbump config

--- dev/dev.R
+++ dev/dev.R
@@ -21,3 +21,8 @@ usethis::use_description(check_name = FALSE)
 usethis::use_namespace()
 
 usethis::use_package("shiny")
+
+# version management
+# remotes::install_github("davidruvolo51/pkgbump")
+pkgbump::set_pkgbump(c("DESCRIPTION", "package.json"))
+pkgbump::pkgbump(version = "1.0.0")
