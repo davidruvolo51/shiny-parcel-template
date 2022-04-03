bd998e5c72216578f732aa620cad8fe400a6426a
davidruvolo51 <davidruvolo51@gmail.com>
2022-04-03 11:23:37 +0200

    rebase; updated version

--- dev/dev.R
+++ dev/dev.R
@@ -2,10 +2,10 @@
 #' FILE: dev.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-03-31
-#' MODIFIED: 2021-03-31
+#' MODIFIED: 2022-04-03
 #' PURPOSE: workspace management
-#' STATUS: in.progress
-#' PACKAGES: usethis
+#' STATUS: stable
+#' PACKAGES: usethis, renv, pkgbump
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
 
@@ -19,4 +19,4 @@ usethis::use_package("shiny")
 # version management
 # remotes::install_github("davidruvolo51/pkgbump")
 pkgbump::set_pkgbump(c("DESCRIPTION", "package.json"))
-pkgbump::pkgbump(version = "1.0.0")
+pkgbump::pkgbump(version = "1.1.0")
